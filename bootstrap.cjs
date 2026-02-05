205b91a96ade38b58bd7fe8ede81624af2bae14e
joningvars <joningvarsson@hotmail.com>
2026-02-05 12:06:11 +0000

    Refactor bootstrap.cjs to utilize URL module for constructing file URLs; update sportfengur-webhooks executable to reflect changes.

--- bootstrap.cjs
+++ bootstrap.cjs
@@ -1,8 +1,9 @@
 // CJS entry for pkg to load the ESM app.
 const path = require('path');
-const { pathToFileURL } = require('url');
+const { URL } = require('url');
 
-const entryUrl = pathToFileURL(path.join(__dirname, 'server.js')).href;
+const entryPath = path.resolve(__dirname, 'server.js').replace(/\\/g, '/');
+const entryUrl = new URL(`file:///${entryPath}`).href;
 import(entryUrl).catch((err) => {
   console.error('Failed to start app:', err);
   process.exit(1);
