2f41c1da050d75c039f78949c1aa71db5c9ea776
joningvars <joningvarsson@hotmail.com>
2026-02-05 11:59:27 +0000

    Refactor bootstrap.cjs to use path and URL modules for dynamic import of server.js; update sportfengur-webhooks executable.

--- bootstrap.cjs
+++ bootstrap.cjs
@@ -1,5 +1,9 @@
 // CJS entry for pkg to load the ESM app.
-import('./server.js').catch((err) => {
+const path = require('path');
+const { pathToFileURL } = require('url');
+
+const entryUrl = pathToFileURL(path.join(__dirname, 'server.js')).href;
+import(entryUrl).catch((err) => {
   console.error('Failed to start app:', err);
   process.exit(1);
 });
